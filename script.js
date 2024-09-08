6a19e2bdb8249a607cde773fae3f17482d34171d
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-08 15:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for(let i=0;i<s1.length;i++){
+		if(s2[0] == s1[i]){
+			return indexOf(s1);
+		}
+		else{
+			return -1;
+		}
+}
+	
+	
 }
 
 // Please do not change the code below
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
